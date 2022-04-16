74dad6cdb92012ccae89fef6e51af6e836b8ff0c
CLDrains <103608931+CLDrains@users.noreply.github.com>
2022-04-16 08:35:37 +0200

    FIXXED IT!

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,19 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azurefuncresumecounter.azurewebsites.net/api/GetResumeCounter?code=js0DHEO4Qhq49j2CP6LhGRy4U3WCdUeB4hJZJtcg5ovoDILRcvJyhg==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(respone => {      
+    fetch(functionApiUrl).then(response => {      
         return response.json()   
-    }).then(respone =>{
+    }).then(response =>{
         console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
-    })
+    });
     return count;
 }
